e464c9ddb09d6beff8da5199339aae98ebfdd680
josemnv <jose.nunez@reclamador.es>
2020-04-17 11:36:50 +0200

    fix pep8

--- django_inlinecss/templatetags/inlinecss.py
+++ django_inlinecss/templatetags/inlinecss.py
@@ -22,7 +22,7 @@ class InlineCssNode(template.Node):
             path = expression.resolve(context, True)
             if path is not None:
                 path = smart_text(path)
-	    if settings.DEBUG:
+            if settings.DEBUG:
                 expanded_path = finders.find(path)
             else:
                 try:
